01cb448beab95db01af42131932d297e7935d61f
Nick <nickmagi24@gmail.com>
2021-11-17 18:18:46 -0500

    Alice in Wonderland Theme

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Card from './components/Card';
 
 
 const cardImages = [
-  { "src": "/img/helmet-1.png", matched: false },
-  { "src": "/img/potion-1.png", matched: false },
-  { "src": "/img/ring-1.png", matched: false },
-  { "src": "/img/scroll-1.png", matched: false },
-  { "src": "/img/shield-1.png", matched: false },
-  { "src": "/img/sword-1.png", matched: false }
+  { "src": "/img/alice.jpg", matched: false },
+  { "src": "/img/cheshire-2.jpg", matched: false },
+  { "src": "/img/hare.jpg", matched: false },
+  { "src": "/img/hatter.jpg", matched: false },
+  { "src": "/img/piller.jpg", matched: false },
+  { "src": "/img/queen.jpg", matched: false }
 ]
 
 
